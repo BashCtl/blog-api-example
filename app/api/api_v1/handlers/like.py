517b6939bad07ad188fee05d85a7d37bccedeaf8
darko <ki14.skakun@gmail.com>
2023-09-05 18:07:57 +0300

    added comment likes

--- app/api/api_v1/handlers/like.py
+++ app/api/api_v1/handlers/like.py
@@ -10,7 +10,13 @@ from app.services.likes_service import LikeService
 likes_router = APIRouter()
 
 
-@likes_router.post("/likes/<post_id>", status_code=status.HTTP_201_CREATED)
+@likes_router.post("/likes/posts/<post_id>", status_code=status.HTTP_201_CREATED)
 def set_like_for_post(post_id: int, current_account: Account = Depends(AuthService.get_current_user),
                       db: Session = Depends(get_db)):
     return LikeService.set_post_like(post_id, current_account, db)
+
+
+@likes_router.post("/likes/comments/<comment_id>", status_code=status.HTTP_201_CREATED)
+def set_like_for_comment(comment_id: int, current_account: Account = Depends(AuthService.get_current_user),
+                         db: Session = Depends(get_db)):
+    return LikeService.set_comment_like(comment_id, current_account, db)
--- app/services/likes_service.py
+++ app/services/likes_service.py
@@ -1,9 +1,10 @@
 from fastapi import HTTPException, status, Response
-from app.schemas import post_schema
 from sqlalchemy.orm import Session
 from app.models.post_model import Post
 from app.models.account_model import Account
 from app.models.post_like_model import PostLike
+from app.models.comments_model import Comment
+from app.models.comment_like_model import CommentLike
 
 
 class LikeService:
@@ -22,5 +23,24 @@ class LikeService:
             return {"message": "Successfully added like."}
         else:
             post_like_query.delete(synchronize_session=False)
+            db.commit()
+            return {"message": "Successfully removed like."}
+
+    @staticmethod
+    def set_comment_like(comment_id: int, account: Account, db: Session):
+        comment = db.query(Comment).filter(Comment.id == comment_id).first()
+        if not comment:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                                detail=f"Comment with id '{comment_id}' not found.")
+
+        comment_like_query = db.query(CommentLike).filter(CommentLike.comment_id == comment_id,
+                                                          CommentLike.author_id == account.id)
+        if not comment_like_query.first():
+            comment_like = CommentLike(comment_id=comment_id, author_id=account.id)
+            db.add(comment_like)
+            db.commit()
+            return {"message": "Successfully added like."}
+        else:
+            comment_like_query.delete(synchronize_session=False)
             db.commit()
             return {"message": "Successfully removed like."}
