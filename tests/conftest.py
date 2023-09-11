560112dee5f2b87865193f7fe0314effebb9ce18
darko <ki14.skakun@gmail.com>
2023-09-11 20:42:08 +0300

    commenting tests and fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,36 +20,36 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-@pytest.fixture
-def session():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@pytest.fixture
-def client(session):
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
-
-
-@pytest.fixture
-def test_account(client):
-    account_data = {"email": "turtles@test.com",
-                    "name": "ninja",
-                    "password": "qwerty1234"}
-    res = client.post("/accounts/", json=account_data)
-    assert res.status_code ==201
-    new_account = res.json()
-    new_account["password"] = account_data["password"]
-    return new_account
+# @pytest.fixture
+# def session():
+#     Base.metadata.drop_all(bind=engine)
+#     Base.metadata.create_all(bind=engine)
+#     db = TestingSessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+#
+#
+# @pytest.fixture
+# def client(session):
+#     def override_get_db():
+#         try:
+#             yield session
+#         finally:
+#             session.close()
+#
+#     app.dependency_overrides[get_db] = override_get_db
+#     yield TestClient(app)
+#
+#
+# @pytest.fixture
+# def test_account(client):
+#     account_data = {"email": "turtles@test.com",
+#                     "name": "ninja",
+#                     "password": "qwerty1234"}
+#     res = client.post("/accounts/", json=account_data)
+#     assert res.status_code ==201
+#     new_account = res.json()
+#     new_account["password"] = account_data["password"]
+#     return new_account
